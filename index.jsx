941f108e8b116f9d69b82c20a90d8f7f4b4e1196
Aleksandr Tarasevich <tarasevich.alex01@mail.ru>
2024-12-28 20:10:50 +0300

    Add program logic

---
+++ index.jsx
@@ -0,0 +1,16 @@
+import {createRoot} from "react-dom/client"
+import React from 'react';
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <>
+        <Navbar/>
+        <Main/>
+
+    </>
+);
+
+// Остановился на 1:55
